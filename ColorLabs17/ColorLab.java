9698fa599fb4ce8a0c5e6cd9b3748507adb20fa4
Imaan Gunja <iagunja@stu.naperville203.org>
2025-11-11 12:06:15 -0600

    color lab

--- ColorLabs17/ColorLab.java
+++ ColorLabs17/ColorLab.java
@@ -31,32 +31,91 @@ public class ColorLab
          Picture pic2 = new Picture("images/beach.jpg");
          Picture pic3 = new Picture("images/beach.jpg");
          Picture pic4 = new Picture("images/beach.jpg");
+         Picture pic5 = new Picture("images/beach.jpg");
+         Picture pic6 = new Picture("images/beach.jpg");
          
          opic.explore();
          
-         Pixel[] beachPixels;
-         beachPixels = pic1.getPixels();
-         
          //CHANGE GREEN
+         Pixel[] bPixels1;
+         bPixels1 = pic1.getPixels();
+         
          int changeGreen;
-         for (Pixel spot1 : beachPixels) {
+         for (Pixel spot1 : bPixels1) {
              changeGreen = spot1.getGreen();
              changeGreen = (int)(changeGreen * 1.5);
              spot1.setGreen(changeGreen);
          }
          pic1.explore();
          
-         //NEGATE
-         
          //GRAYSCALE
+         Pixel[] bPixels2;
+         bPixels2 = pic2.getPixels();
+         
          int aRed, aBlue, aGreen, avgColor;
-         for (Pixel spot1 : beachPixels) {
+         for (Pixel spot1 : bPixels2) {
              aRed = spot1.getRed();
              aBlue = spot1.getBlue();
              aGreen = spot1.getGreen();
              
-             avgColor = ((aRed + aBlue + aGreen) / 3);
+             avgColor = (int)((aRed + aBlue + aGreen) / 3);
+             int gRed = avgColor;
+             int gBlue = avgColor;
+             int gGreen = avgColor;
 
+             Color gray = new Color(gRed, gBlue, gGreen);
+             
+             spot1.setColor(gray);
+         }
+         pic2.explore();
+         
+         //NEGATE
+         Pixel[] bPixels3;
+         bPixels3 = pic3.getPixels();
+         
+         int pRed, pBlue, pGreen;
+         for (Pixel spot1 : bPixels3) {
+             pRed = spot1.getRed();
+             pBlue = spot1.getBlue();
+             pGreen = spot1.getGreen();
+             
+             int nRed = 255 - pRed;
+             int nBlue = 255 - pBlue;
+             int nGreen = 255 - pGreen;
+             
+             Color negColor = new Color(nRed, nBlue, nGreen);
+             
+             spot1.setColor(negColor);
+         }
+         pic3.explore();
+         
+         //DARKEN
+         Pixel[] bPixels4;
+         bPixels4 = pic4.getPixels();
+         
+         int NRed, NBlue, NGreen;
+         for (Pixel spot1 : bPixels4) {
+             NRed = spot1.getRed();
+             NBlue = spot1.getBlue();
+             NGreen = spot1.getGreen();
+             
+             int dRed = (int)(NRed / 2);
+             int dBlue = (int)(NBlue / 2);
+             int dGreen = (int)(NGreen / 2);
+             
+             Color darkColor = new Color(dRed, dBlue, dGreen);
+             
+             spot1.setColor(darkColor);
+         }
+         pic4.explore();
+         
+         //CHANGE OBJECT COLOR
+         Pixel[] bPixels5;
+         bPixels5 = pic5.getPixels();
+         
+         int changeSky;
+         for (Pixel spot1 : bPixels5) {
+             
          }
     }//main       
 }//class
