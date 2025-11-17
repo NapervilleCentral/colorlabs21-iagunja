52c90d95a2fa7c1bc9c182d1e9890387ab3cccd1
Imaan Gunja <iagunja@stu.naperville203.org>
2025-11-17 12:07:12 -0600

    commit

--- ColorLabs17/ShepardFaireyLab.java
+++ ColorLabs17/ShepardFaireyLab.java
@@ -25,6 +25,7 @@ public class ShepardFaireyLab
          Picture meGray = new Picture("images/imaan.jpg");
          Picture me1 = new Picture("images/imaan.jpg");
          Picture me2 = new Picture("images/imaan.jpg");
+         Picture me3 = new Picture("images/imaan.jpg");
          
          opic.explore();
          
@@ -55,9 +56,16 @@ public class ShepardFaireyLab
          Pixel[] mePixels1;
          mePixels1 = me1.getPixels();
          
-         int sRed;
+         int sRed, sBlue, sGreen, avg1;
          for (Pixel spot1 : mePixels1) {
              sRed = spot1.getRed();
+             sBlue = spot1.getBlue();
+             sGreen = spot1.getGreen();
+             avg1 = (sRed + sBlue + sGreen) / 3;
+             
+             Color gray1 = new Color(avg1, avg1, avg1);
+             
+             spot1.setColor(gray1);
              
              Color darkBlue = new Color(0, 76, 153);//dark blue
              Color red = new Color(204, 0, 0);//red
@@ -87,10 +95,91 @@ public class ShepardFaireyLab
          Pixel[] mePixels2;
          mePixels2 = me2.getPixels();
          
-         /**
-          * custom color palette
-          */
-
+         int s = 255;
+         int b = 0;
+         int sRed2, sBlue2, sGreen2, avg2;
+         for (Pixel spot1 : mePixels2) {
+             sRed2 = spot1.getRed();
+             sBlue2 = spot1.getBlue();
+             sGreen2 = spot1.getGreen();
+             avg2 = (sRed2 + sBlue2 + sGreen2) / 3;
+             
+             Color gray2 = new Color(avg2, avg2, avg2);
+             
+             spot1.setColor(gray2);
          
-    }//main       
-}//class
+             if (spot1.getRed() < s) {
+                 s = spot1.getRed();
+             }
+             if (spot1.getRed() > b) {
+                 b = spot1.getRed();
+             }
+         }//main   
+         
+         double interval = (double)(b - s) / 4.0;
+         
+         Color darkBlue = new Color(0, 76, 153);//dark blue
+         Color red = new Color(204, 0, 0);//red
+         Color lightBlue = new Color(204, 229, 255);//light blue
+         Color offWhite = new Color(230, 220, 199);//off white
+         
+         int grayVal;
+         
+         for (Pixel spot1 : mePixels2) {
+             grayVal = spot1.getRed();
+             
+             if (grayVal >= s && grayVal < s + interval) {
+                 spot1.setColor(darkBlue);
+             }
+             if (grayVal >= s + interval && grayVal < s + 2 * interval) {
+                 spot1.setColor(red);
+             }
+             if (grayVal >= s + 2 * interval && grayVal < s + 3 * interval) {
+                 spot1.setColor(lightBlue);
+             }
+             if (grayVal >= s + 3 * interval && grayVal < s + 4 * interval) {
+                 spot1.setColor(offWhite);
+             }
+         }
+         me2.explore();
+         
+         /**
+             * custom color palette
+         */
+        
+         Pixel[] mePixels3;
+         mePixels3 = me3.getPixels();
+         
+         int sRed3, sBlue3, sGreen3, avg3;
+         for (Pixel spot1 : mePixels3) {
+             sRed3 = spot1.getRed();
+             sBlue3 = spot1.getBlue();
+             sGreen3 = spot1.getGreen();
+             avg3 = (sRed3 + sBlue3 + sGreen3) / 3;
+             
+             Color gray3 = new Color(avg3, avg3, avg3);
+             
+             spot1.setColor(gray3);
+             
+             Color pink = new Color(255, 226, 241);//light pink
+             Color yellow = new Color(255, 253, 205);//light yellow
+             Color blue = new Color(207, 241, 252);//light blue
+             Color cream = new Color(255, 255, 243);//cream
+             
+             if (sRed3 >= 0 && sRed3 <= 63) {
+                 spot1.setColor(pink);
+             }
+             if (sRed3 > 63 && sRed3 <= 127) {
+                 spot1.setColor(cream);
+             }
+             if (sRed3 > 127 && sRed3 <= 191) {
+                 spot1.setColor(blue);
+             }
+             if (sRed3 > 191 && sRed3 <= 255) {
+                 spot1.setColor(yellow);
+             }
+         }
+         me3.explore();
+         me3.write("images/SFtryFinal.jpg");
+    }//class
+}
